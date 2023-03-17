02816fdd54c8b65ebe7c83f3d3f57b6ba76d4fa3
Ivan Pivnev <pivnev-1998@mail.ru>
2023-03-17 11:45:46 +0300

    DZ9C

---
+++ Primeri/Primer_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Задайте значения M и N. Напишите программу, которая выведет
+//  все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+NaturalToLow(m, n);
+
+void NaturalToLow(int m, int n)
+{
+    if (m > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow( m + 1, n);
+        Console.Write($"{m}" + " ");
+    }
+}
